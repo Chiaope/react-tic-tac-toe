ac588c4ea16a0800bd4c6231e49d77dbe8d1b590
gui kang <gui_kang@hotmail.com>
2024-07-29 21:00:45 +0800

    allow game board to be populated with the different player symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react"
 import GameBoard from "./components/GameBoard/GameBoard"
 import PlayerListItem from "./components/PlayerListItem/PlayerListItem"
 
-function App() {
 
+let player1Symbol = 'X'
+let player2Symbol = 'O'
+
+function derivePlayer(gameTurns) {
+  let player = player1Symbol
+  if (gameTurns.length > 0 && gameTurns[0].player === player1Symbol) {
+    player = player2Symbol
+  }
+  return player
+}
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([])
+
+  let activePlayer = derivePlayer(gameTurns)
+
+  function handleOnSelect(rowIndex, colIndex) {
+    setGameTurns((prevGameTurns) => {
+      const currentPlayer = derivePlayer(prevGameTurns)
+      console.log('current player')
+      console.log(currentPlayer)
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevGameTurns]
+
+      return updatedTurns
+    })
+  }
 
   return (
     <main>
       <div id='game-container'>
         <ol id='players'>
-          <PlayerListItem initialName={'Player 1'} symbol={'X'}/>
-          <PlayerListItem initialName={'Player 2'} symbol={'O'}/>
+          <PlayerListItem initialName={'Player 1'} symbol={player1Symbol} />
+          <PlayerListItem initialName={'Player 2'} symbol={player2Symbol} />
         </ol>
-        <GameBoard />
+        <GameBoard turns={gameTurns} onSelect={handleOnSelect} />
       </div>
       LOG
     </main>
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -4,9 +4,16 @@ let initialGameBoard = [
     [null, null, null]
 ]
 
-
-export default function GameBoard() {
+export default function GameBoard({ turns, onSelect }) {
     let gameBoard = initialGameBoard
+    for (const turn of turns) {
+        console.log(turn)
+        const { square, player } = turn
+        const { row, col } = square
+        gameBoard[row][col] = player
+    }
+    console.log(gameBoard)
+
     return (
         <ol id='game-board'>
             {gameBoard.map((row, rowIndex) => (
@@ -14,7 +21,7 @@ export default function GameBoard() {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => onSelect(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
